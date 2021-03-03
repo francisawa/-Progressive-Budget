ddff27cc7925171732112dc635e79af4843d268d
Awa <francisawa2@gmail.com>
2021-03-02 17:09:53 -0800

    service worker line2 update

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,10 +1,11 @@
 const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/index.js",
-  "/styles.css",
-  "/db.js"
+'/index.html',
+'/styles.css',
+'/index.js',
+'/icons/icon-192x192.png',
+'/icons/icon-512x512.png'
 ];
+
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
